9a1f434f1d069de4df6fc5248241160b1c04a9c8
Tom Malone <tmalone+git@gmail.com>
2022-05-20 05:21:52 -0400

    Added deta AsyncBase back to main app

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 """
 """
-from fastapi import FastAPI
+import fastapi
+from deta import Deta
 
-app = FastAPI()
+from .src.mapapp.geojson.schema import Point
+
+# config - for now, just put everything in a single file, until errors are eliminated.
+db_config = {
+    # DB naming convention: "project_app_items", where `items` = whatever is to be stored.
+	'locations': "execas_mapapp_locations"
+}
+deta = Deta()
+async_db = deta.AsyncBase(db_config["locations"])
+app = fastapi.FastAPI()
+
 
 # define root route
 # (i.e. the home page, or "/")
 @app.get("/")
 async def root():
     return "Hello world!"
+
